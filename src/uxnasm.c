3d65c5f572d46bc748aefb3f726ec4088d8916cb
Devine Lu Linvega <aliceffekt@gmail.com>
2024-03-29 10:49:26 -0700

    Improved error message

--- src/uxnasm.c
+++ src/uxnasm.c
@@ -23,7 +23,7 @@ typedef struct { int line; char *path; } Context;
 
 static int ptr, length;
 static char token[0x40], scope[0x40], lambda[0x05];
-static char dict[0x4000], *dictnext = dict;
+static char dict[0x8000], *dictnext = dict;
 static Uint8 data[0x10000], lambda_stack[0x100], lambda_ptr, lambda_len;
 static Uint16 labels_len, refs_len, macro_len;
 static Item labels[0x400], refs[0x1000], macros[0x100];
@@ -40,7 +40,7 @@ static char ops[][4] = {
 static int   find(char *s, char t) { int i = 0; char c; while((c = *s++)) { if(c == t) return i; i++; } return -1; } /* chr in str */
 static int   shex(char *s) { int n = 0; char c; while((c = *s++)) { if(find(hexad, c) < 0) return -1; n = n << 4, n |= find(hexad, c); } return n; } /* str to hex */
 static int   scmp(char *a, char *b, int len) { int i = 0; while(a[i] == b[i]) if(!a[i] || ++i >= len) return 1; return 0; } /* str compare */
-static char *scpy(char *src, char *dst, int len) { int i = 0; while((dst[i] = src[i]) && i < len - 2) i++; dst[i + 1] = '\0'; return dst; } /* str copy */
+static char *copy(char *src, char *dst, char c) { while(*src && *src != c) *dst++ = *src++; *dst++ = 0; return dst; } /* str copy */
 static char *save(char *s, char c) { char *o = dictnext; while((*dictnext++ = *s++) && *s); *dictnext++ = c; return o; } /* save to dict */
 static char *join(char *a, char j, char *b) { char *res = dictnext; save(a, j), save(b, 0); return res; } /* join two str */
 
@@ -97,7 +97,7 @@ findopcode(char *s)
 			else if(s[m] == 'k')
 				i |= (1 << 7);
 			else
-				return 0;
+				return error_top("Unknown opcode mode", s);
 			m++;
 		}
 		return i;
@@ -177,7 +177,7 @@ makemacro(char *name, FILE *f, Context *ctx)
 		if(c == 0xa) ctx->line += 1;
 	while(f && fread(&c, 1, 1, f) && c != '}') {
 		if(c == 0xa) ctx->line += 1;
-		if(c == '%') return 0;
+		if(c == '%') return error_top("Nested macro", name);
 		if(c == '(')
 			walkcomment(f, ctx);
 		else
@@ -200,12 +200,7 @@ makelabel(char *name, int setscope, Context *ctx)
 	l->name = push(name, 0);
 	l->addr = ptr;
 	l->refs = 0;
-	if(setscope) {
-		int i = 0;
-		while(name[i] != '/' && i < 0x3e && (scope[i] = name[i]))
-			i++;
-		scope[i] = '\0';
-	}
+	if(setscope) copy(name, scope, '/');
 	return 1;
 }
 
@@ -263,9 +258,9 @@ writehex(char *w, Context *ctx)
 {
 	if(*w == '#')
 		writebyte(findopcode("LIT") | !!(++w)[2] << 5, ctx);
-	if(!w[2])
+	if(w[1] && !w[2])
 		return writebyte(shex(w), ctx);
-	else if(!w[4])
+	else if(w[3] && !w[4])
 		return writeshort(shex(w));
 	else
 		return 0;
@@ -336,7 +331,7 @@ resolve(void)
 	for(i = 0; i < refs_len; i++) {
 		Item *r = &refs[i], *l = findlabel(r->name);
 		Uint8 *rom = data + r->addr;
-		if(!l) return 0;
+		if(!l) return error_top("Unknown label", r->name);
 		switch(r->rune) {
 		case '_':
 		case ',':
@@ -404,9 +399,9 @@ int
 main(int argc, char *argv[])
 {
 	ptr = PAGE;
-	scpy("on-reset", scope, 0x40);
+	copy("on-reset", scope, 0);
 	if(argc == 1) return error_top("usage", "uxnasm [-v] input.tal output.rom");
-	if(scmp(argv[1], "-v", 2)) return !fprintf(stdout, "Uxnasm - Uxntal Assembler, 28 Mar 2024.\n");
+	if(scmp(argv[1], "-v", 2)) return !fprintf(stdout, "Uxnasm - Uxntal Assembler, 29 Mar 2024.\n");
 	if(!assemble(argv[1]) || !length) return !error_top("Assembly", "Failed to assemble rom.");
 	if(!resolve()) return !error_top("Assembly", "Failed to resolve symbols.");
 	if(!build(argv[2])) return !error_top("Assembly", "Failed to build rom.");
